08d69b8f9fc44cdd0e5bc1ea106289303104b7a9
sander.rikka <sander.rikka@gmail.com>
2025-02-17 17:03:50 +0200

    2025.02.17 17.00 local - train_nans.ipynb testing for logarithmic data for training. - hiiu_liivi_data.csv test data (but it seems to include some errors)

--- src/models/avely_lstm.py
+++ src/models/avely_lstm.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+
+from sklearn.metrics import r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Reshape
 from tensorflow.keras.losses import categorical_crossentropy
@@ -15,7 +17,7 @@ def simple_lstm_v1(input_size):
     #model.add(LSTM(32, activation='tanh'))
     model.add(Dense(1, activation='relu'))
 
-    model.compile(optimizer='adam', loss='mae')
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
     model.summary()
 
     return model
@@ -33,7 +35,7 @@ def train_model(model, X_train, y_train, mdl_name=None):
     # Train the model
     history = model.fit(X_train,
                         y_train,
-                        epochs=150,
+                        epochs=100,
                         batch_size=32,
                         validation_split=0.3)
 
